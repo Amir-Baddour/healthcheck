678f34d325f44ee34299241493f7db7ac836ab5c
Amir Baddour <amirbaddour675@gmail.com>
2026-04-09 23:46:05 +0300

    update healthcheck message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         port(8080);
 
-        get("/healthcheck", (req, res) -> "AMIR'S SERVER IS LIVE AND ON PROD");
+        get("/healthcheck", (req, res) -> "AMIR'S SERVER IS LIVE AND ON PROD - version 2");
     }
 }
