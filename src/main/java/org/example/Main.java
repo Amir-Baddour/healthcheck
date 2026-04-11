6d52d7ac65a351540b456e7f7f01eb85843ca4b9
Amir Baddour <amirbaddour675@gmail.com>
2026-04-11 20:46:15 +0300

    logger import

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,19 @@
 package org.example;
 
 import static spark.Spark.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    private static final AtomicInteger requestCount = new AtomicInteger(0);
+
     public static void main(String[] args) {
         port(8080);
-
-        get("/healthcheck", (req, res) -> "AMIR'S SERVER IS LIVE AND ON PROD - version 2");
+        get("/healthcheck", (req, res) -> {
+            int count = requestCount.incrementAndGet();
+            logger.info("Healthcheck hit #" + count + " from " + req.ip());
+            return "AMIR'S SERVER IS LIVE AND ON PROD - version 2";
+        });
     }
 }
